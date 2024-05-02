09af2c7df217f4e697543e8a7021cea0b4fafb6c
Fàtima <104974181+metodes@users.noreply.github.com>
2024-05-02 16:37:55 +0200

    Update montecarlo.py

--- montecarlo.py
+++ montecarlo.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+N = 10
+K_b = 1.380649e-23
+A = 1.0
+
+
+def it_estats():
+    return range(1, N+1)
+
+
+def energia(estat: int) -> float:
+    return A * estat * K_b
+import numpy as np
+
 Num_estats = 10
 K_b = 1.380649e-23
 A = 1.0
@@ -15,7 +28,8 @@ def energia(estat: int) -> float:
 
 def paso_montecarlo(estat_actual: int, temp: float) -> int:
 
-    # Realitza un cicle de Monte Carlo en l'estat actual. Torna el nou estat després del moviment.
+    #Realitza un cicle de Monte Carlo en l'estat actual.
+    #Torna el nou estat després del moviment.
 
     # Genera un nou estat aleatori entre 1 y 10
     nou_estat = np.random.randint(1, Num_estats+1)
@@ -30,17 +44,17 @@ def paso_montecarlo(estat_actual: int, temp: float) -> int:
         return estat_actual
 
 
-temperatures = [0.01, 0.1, 70, 700]
+temperatures = [0.01, 0.06, 0.1, 0.4, 0.6, 10, 40, 100, 400]
 Num_pasos = 100000
 
 for temp in temperatures:
     print("==============================")
-    print(f"====== TEMPERATURA {temp} ======")
+    print(f"====== TEMPERATURA= {temp} ======")
 
     # Inicialitza l'estat arbitrari
     estat_actual = np.random.randint(1, Num_estats+1)
 
-    # Contador per registrar la ocupació de cada estat
+    # Contadors per registrar la ocupació de cada estat
     cont_estats = {estat: 0 for estat in it_estats()}
 
     # Realitza la simulació de Monte Carlo
